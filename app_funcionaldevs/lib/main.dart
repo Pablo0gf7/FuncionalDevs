4d66bd4da0b087239000c429b125753cc58377a6
Ruben <rubengirela@gmail.com>
2022-11-05 19:34:29 +0100

    Cambiado nombres clases

--- app_funcionaldevs/lib/main.dart
+++ app_funcionaldevs/lib/main.dart
@@ -16,14 +16,14 @@ class MyApp extends StatelessWidget {
     // ignore: prefer_const_constructors
     return MaterialApp(
       //titulo de la app
-      title: 'Startup Name Generator 2',
+      title: 'FuncionalDevs',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.white,
           foregroundColor: Colors.black,
         ),
       ),
-      home: RandomWords(),
+      home: TaskList(),
     );
   }
 }
@@ -51,27 +51,7 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'My Flutter Demo First'),*/
 
-//DESABILITADO------------------------------------------------------------------
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-//------------------------------------------------------------------------------
-
-class RandomWordsState extends State<RandomWords> {
+class TaskListState extends State<TaskList> {
   //array que genera las palabras aleatorias
   final _suggestions = <String>[];
   //tamaño del texto de la lista
@@ -113,7 +93,7 @@ class RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       //añado un icono a la barra AppBar
-      appBar: AppBar(title: const Text('Starup Name Generator 5'), actions: [
+      appBar: AppBar(title: const Text('Lista de tareas'), actions: [
         IconButton(
           icon: const Icon(Icons.list),
           onPressed: _pushSaved,
@@ -189,81 +169,9 @@ class RandomWordsState extends State<RandomWords> {
   // TODO Add build() method
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({super.key});
+class TaskList extends StatefulWidget {
+  const TaskList({super.key});
 
   @override
-  State<RandomWords> createState() => RandomWordsState();
-}
-
-//DESABILITADO------------------------------------------------------------------
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    //genera palabras inglesas aleatorias
-    //final wordPair = WordPair.random();
-
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have clicked the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            RandomWords(),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment pls',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  State<TaskList> createState() => TaskListState();
 }
