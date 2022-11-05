5b31904a98fec6255b61e2ba284f65077960af5a
Ruben <rubengirela@gmail.com>
2022-11-05 19:22:06 +0100

    Añadido lista basica, tengo que modificarla pero para tener algo

--- app_funcionaldevs/lib/main.dart
+++ app_funcionaldevs/lib/main.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() {
+/*void main() {
   runApp(const MyApp());
-}
+}*/
+//es lo mismo que hacer
+void main() => runApp(const MyApp());
 
+//clase principal
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
     return MaterialApp(
-      title: 'Flutter Demo',
+      //titulo de la app
+      title: 'Startup Name Generator 2',
       theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: RandomWords(),
+    );
+  }
+}
+/*Scaffold(
+          appBar: AppBar(
+            //titulo del banner
+            title: const Text('Startup Name Generator 3'),
+          ),
+          body: const Center(
+            //clase que generara la lista de palabras random
+            child: RandomWords(),
+          ),
+        )*/
+/*theme: ThemeData(
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -24,11 +49,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: const MyHomePage(title: 'My Flutter Demo First'),*/
 
+//DESABILITADO------------------------------------------------------------------
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -46,7 +69,134 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+//------------------------------------------------------------------------------
 
+class RandomWordsState extends State<RandomWords> {
+  //array que genera las palabras aleatorias
+  final _suggestions = <String>[];
+  //tamaño del texto de la lista
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  //Funcion que va a otra pagina de la app
+  void _pushSaved() {
+    /*Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _saved.map(
+        (pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        },
+      );
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList()
+          : <Widget>[];
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Saved Sugestions'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: divided,
+        ),
+      );
+    }));*/
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //añado un icono a la barra AppBar
+      appBar: AppBar(title: const Text('Starup Name Generator 5'), actions: [
+        IconButton(
+          icon: const Icon(Icons.list),
+          onPressed: _pushSaved,
+          tooltip: 'Sugerencias guardadas',
+        )
+      ]),
+      //creo la listView
+      body: ListView.builder(
+          //padding entre items
+          padding: const EdgeInsets.all(16.0),
+          /*
+    se llama cada par de palabras sugeridas, añadiendo una fila para las parejas 
+    de palabras.
+    */
+          itemBuilder: (context, i) {
+            //construye la lista
+            //añade un widget separador entre items de cada fila en la ListView.
+            if (i.isOdd) return const Divider();
+
+            /*
+      divide i entre 2 de forma entera, para calcular el numero de palabras 
+      emparejadas en la ListView, salvo los widgets divisores.
+      */
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              /*
+        si alcanza el final de los pares de palabras, entonces genera 10 más y
+        los añade a la lista de sugerencias.
+        */
+              _suggestions.addAll([
+                'task 1',
+                'task 2',
+                'task3',
+                'task 4',
+                'task 5',
+                'task6',
+                'task 7',
+                'task 8',
+                'task 9',
+                'task10'
+              ]);
+            }
+            //guardo las palabras en un contenedor
+            //final alreadySaved = _saved.contains(_suggestions[index]);
+
+            return ListTile(
+              title: Text(
+                _suggestions[index],
+                style: _biggerFont,
+              ),
+              //añadimos el icono del corazon en la lista
+              trailing: Icon(
+                //cuando esta seleccionado Icons.favorite
+                //cuando no esta seleccionado Icons.favorite_border
+                Icons.favorite,
+                //cuando esta seleccionado es rojo y cuando no no tiene color
+                color: Colors.red,
+              ),
+
+              //aplicamos la acción
+              onTap: () {
+                setState(() {
+                  //si esta guardado
+                });
+              },
+            );
+          }),
+    );
+
+    //final wordPair = WordPair.random();
+    //return Text(wordPair.asPascalCase);
+  }
+  // TODO Add build() method
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => RandomWordsState();
+}
+
+//DESABILITADO------------------------------------------------------------------
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -63,6 +213,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    //genera palabras inglesas aleatorias
+    //final wordPair = WordPair.random();
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -96,18 +249,19 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have clicked the button this many times:',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            RandomWords(),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Increment pls',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
