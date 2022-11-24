cfc2e76cf44643318081b96ecc4117b886a4b7ae
Ruben <rubengirela@gmail.com>
2022-11-24 12:23:24 +0100

    Añadido menu parcialmente

--- app_funcionaldevs/lib/main.dart
+++ app_funcionaldevs/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 /*void main() {
   runApp(const MyApp());
@@ -24,13 +25,359 @@ class MyApp extends StatelessWidget {
           foregroundColor: Colors.black,
         ),
       ),
-      home: TaskList(),
+      home: MenuList(),
     );
   }
 }
 
-Future<http.Response> fetchTareas(http.Client client) async {
+/*Future<http.Response> fetchTareas(http.Client client) async {
   return client.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+}*/
+
+//creo el contenido visual y la acción a hacer del menu
+class MenuListState extends State<MenuList> {
+  //tamaño del texto de la lista
+  //final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  //Funcion que va a otra pagina de la app
+  void _pushTask() {
+    /*Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _saved.map(
+        (pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        },
+      );
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList()
+          : <Widget>[];
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Saved Sugestions'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: divided,
+        ),
+      );
+    }));*/
+  }
+  //OTRA FORMA PERO ES MAS ENREDESADA
+  //https://api.flutter.dev/flutter/material/ListTile-class.html
+
+  //JSON TERMINARLO
+  //late final String _id;
+
+  /*Future<String> recibirString() async {
+    final respuesta =
+        await http.get("http://tu-ip-o-dominio:8888/codigos/php/");
+    if (respuesta.statusCode == 200) {
+      log(respuesta.body.toString());
+      setState(() {
+        var parsedJson = json.decode(respuesta.body);
+        _id = parsedJson["producto"];
+      });
+    } else {
+      throw Exception("Fallo");
+    }
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    //lista de tipos de opciones
+    final List<int> type_menu = [0, 1, 2, 2, 1, 0, 4, 4];
+    //lista de tareas
+    final List<String> items = [
+      "Lista de tareas",
+      "Tareas realizadas",
+      "Chat",
+      "Diagrama de desarrollo",
+      "Inventario"
+    ];
+
+    //genero la listview separable
+    final lista_menu = ListView.separated(
+      separatorBuilder: (context, index) => Divider(
+        color: Color.fromARGB(255, 243, 245, 243),
+      ),
+      padding: const EdgeInsets.all(26.0), //paddind
+      itemCount: items.length, //numero de elementos a tener
+      itemBuilder: (context, index) {
+        //dependiendo del tipo de tarea cambiara el icono
+        switch (type_menu[index]) {
+          case 0:
+            return Expanded(
+              child: Container(
+                width: 200,
+                height: 100,
+                color: Color.fromARGB(255, 161, 218, 233), //color
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: 100,
+                        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            items[index],
+                            maxLines: 3,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 35,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('images/print.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: BoxShape.rectangle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+
+            break;
+          case 1:
+            return Expanded(
+              child: Container(
+                width: 200,
+                height: 100,
+                color: Color.fromARGB(255, 161, 218, 233), //color
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: 100,
+                        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            items[index],
+                            maxLines: 3,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 35,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('images/example.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: BoxShape.rectangle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+            break;
+          case 2:
+            return Expanded(
+              child: Container(
+                width: 200,
+                height: 100,
+                color: Color.fromARGB(255, 161, 218, 233), //color
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: 100,
+                        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            items[index],
+                            maxLines: 3,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 35,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('images/print.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: BoxShape.rectangle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+            break;
+          default:
+            return Expanded(
+              child: Container(
+                width: 200,
+                height: 100,
+                color: Color.fromARGB(255, 161, 218, 233), //color
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: 100,
+                        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            items[index],
+                            maxLines: 3,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 35,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('images/example.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: BoxShape.rectangle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+            break;
+        }
+      },
+    );
+
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 243, 245, 243),
+        //añado un icono a la barra AppBar
+        appBar: AppBar(title: const Text('Menú Principal'), actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushTask, //cuando pincha una tarea (ACCION)
+            tooltip: 'Sugerencias guardadas',
+          )
+        ]),
+        //navegador de abajo
+        bottomNavigationBar: BottomAppBar(
+          child: Padding(
+            padding: const EdgeInsets.all(0),
+            child: OverflowBar(
+              overflowAlignment: OverflowBarAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment:
+                      MainAxisAlignment.center, //alineamiento de las cajas
+                  crossAxisAlignment:
+                      CrossAxisAlignment.center, //alineamiento cruzado
+                  children: <Widget>[
+                    //flecha izquierda
+                    SizedBox(
+                      width: 90,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: Color.fromARGB(255, 4, 127, 158),
+                        ),
+                        iconSize: 80,
+                        onPressed: () {},
+                      ),
+                    ),
+                    SizedBox(width: 50), //separador
+                    //flecha arriba
+                    SizedBox(
+                      width: 90,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_upward,
+                          color: Color.fromARGB(255, 4, 127, 158),
+                        ),
+                        iconSize: 80,
+                        onPressed: () {},
+                      ),
+                    ),
+                    SizedBox(width: 50), //separador
+                    //flecha abajo
+                    SizedBox(
+                      width: 90,
+                      child: IconButton(
+                        style: TextButton.styleFrom(
+                          minimumSize: Size.zero, // Set this
+                          padding: EdgeInsets.zero, // and this
+                        ),
+                        icon: Icon(
+                          Icons.arrow_downward,
+                          color: Color.fromARGB(255, 4, 127, 158),
+                        ),
+                        iconSize: 80,
+                        onPressed: () {},
+                      ),
+                    ),
+                    SizedBox(width: 50), //separador
+                    //flecha derecha
+                    SizedBox(
+                      width: 90,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_forward,
+                          color: Color.fromARGB(255, 4, 127, 158),
+                        ),
+                        iconSize: 80,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          color: Color.fromARGB(255, 168, 15, 148),
+        ),
+        //creo la listView
+        body: Center(child: lista_menu));
+  }
+
+  // TODO Add build() method
+  //acción del boton
+  void accionBoton() {}
 }
 
 //creo el contenido visual y la acción a hacer
@@ -70,12 +417,34 @@ class TaskListState extends State<TaskList> {
       );
     }));*/
   }
-
   //OTRA FORMA PERO ES MAS ENREDESADA
   //https://api.flutter.dev/flutter/material/ListTile-class.html
 
+  //JSON TERMINARLO
+  //late final String _id;
+
+  /*Future<String> recibirString() async {
+    final respuesta =
+        await http.get("http://tu-ip-o-dominio:8888/codigos/php/");
+    if (respuesta.statusCode == 200) {
+      log(respuesta.body.toString());
+      setState(() {
+        var parsedJson = json.decode(respuesta.body);
+        _id = parsedJson["producto"];
+      });
+    } else {
+      throw Exception("Fallo");
+    }
+  }*/
+
   @override
   Widget build(BuildContext context) {
+    //creamos el request
+    //obtenemos el contenido de la pagina
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/photos');
+    //obtengo respuesta
+    //Response response = http.get(url);
+
     //lista de tipos de tareas
     final List<int> type_task = [0, 1, 2, 2, 1, 0, 4, 4];
     //lista de tareas
@@ -373,4 +742,12 @@ class TaskList extends StatefulWidget {
   @override
   State<TaskList> createState() =>
       TaskListState(); //GENERA EL CONTENIDO VISUAL Y EL FUNCIONAMIETO
+}
+
+class MenuList extends StatefulWidget {
+  const MenuList({super.key});
+
+  @override
+  State<MenuList> createState() =>
+      MenuListState(); //GENERA EL CONTENIDO VISUAL Y EL FUNCIONAMIETO
 }
